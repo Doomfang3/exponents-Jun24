e10207d9c4b104bd7ed3668c3a0e4a31d604c7b2
Doomfang3 <saubinmathieu@gmail.com>
2024-07-01 13:00:18 +0200

    Working example

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,42 @@
-import "./App.css";
-import Counter from "./components/Counter";
-import ExponentTwo from "./components/ExponentTwo";
-import ExponentThree from "./components/ExponentThree";
-import ExponentFour from "./components/ExponentFour";
-import ExponentFive from "./components/ExponentFive";
-import ExponentSix from "./components/ExponentSix";
+import './App.css'
+import Counter from './components/Counter'
+import ExponentTwo from './components/ExponentTwo'
+import ExponentThree from './components/ExponentThree'
+import ExponentFour from './components/ExponentFour'
+import ExponentFive from './components/ExponentFive'
+import ExponentSix from './components/ExponentSix'
+import { useState } from 'react'
+import Exponent from './components/Exponent'
 
+function App() {
+  const [count, setCount] = useState(0)
 
-function App () {
+  const decrement = () => setCount(prevCount => prevCount - 1)
+  const increment = () => setCount(prevCount => prevCount + 1)
+
   return (
-    <div className="App">
-      <h2><em>Counter</em></h2>
-  
-      <Counter/>
+    <div className='App'>
+      <h2>
+        <em>Counter</em>
+      </h2>
+
+      <Counter count={count} decrement={decrement} increment={increment} />
 
       <br />
-      <h2><em>Exponents</em></h2>
+      <h2>
+        <em>Exponents</em>
+      </h2>
 
-      <div className="container">
-        <ExponentTwo />
-        <ExponentThree />
-        <ExponentFour />
-        <ExponentFive />
-        <ExponentSix />
+      <div className='container'>
+        <Exponent count={count} exponent={42} />
+        <ExponentTwo count={count} />
+        <ExponentThree count={count} />
+        <ExponentFour count={count} />
+        <ExponentFive count={count} />
+        <ExponentSix count={count} />
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Exponent.jsx
@@ -0,0 +1,14 @@
+const Exponent = ({ count, exponent }) => (
+  <div className='exponent-counter-container'>
+    <p className='exponent-label'>
+      {count}
+      <sup>{exponent}</sup>
+    </p>
+    <p className='exponent-result'>
+      {Array.from({ length: exponent }, () => count).join(' * ')} ={' '}
+      <span className='total'>{count ** exponent}</span>
+    </p>
+  </div>
+)
+
+export default Exponent
